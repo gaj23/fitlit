1a318895d1cd9c992c203c2fefb6ae61b293de42
Thao Ma <MaThao@gmail.com>
2020-12-09 15:47:25 -0700

    Add comparison of user step goal and all user avg step goal to the DOM

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,23 @@
 let userName = document.querySelector('.greeting');
+let stepGoalComparison = document.querySelector('.step-goal-comparison');
 
 const allUser = new UserRepository(userData);
 let user = new User(allUser.getUser(12));
 
-window.addEventListener('load', greetUser);
+window.addEventListener('load', displaySummaryData);
+
+function displaySummaryData() {
+  greetUser();
+  compareStepGoals();
+}
 
 function greetUser() {
-  console.log(user)
-  userName.innerText = `Welcome, ${user.returnFirstName()}!`
+  userName.innerText = `Welcome, ${user.returnFirstName()}!`;
 }
+
+function compareStepGoals() {
+  const avgGoal = allUser.calculateAvgStepGoal();
+  const userGoal = user.dailyStepGoal;
+  stepGoalComparison.innerText = ` Your step goal is: ${userGoal}. Across all users the average step goal is: ${avgGoal}.`;
+}
+
