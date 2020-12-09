0f158a081ce68d7c796ca799054d260b07ffa7e6
Thao Ma <MaThao@gmail.com>
2020-12-08 19:22:37 -0700

    Add describe block

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -2,3 +2,8 @@ const chai = require("chai");
 const expect = chai.expect;
 
 const UserRepository = require('../src/users-repository');
+
+describe('UserRepository', () => {
+
+  it('should be an instance of ')
+});
