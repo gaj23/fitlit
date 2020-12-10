c711086a767b3a74e011b70562ec6147394e277d
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2020-12-09 20:54:49 -0500

    Require files necessary to run test and prepare format

---
+++ test/Hydration-test.js
@@ -0,0 +1,66 @@
+const chai = require("chai");
+const expect = chai.expect;
+
+const Hydration = require('../src/Hydration');
+const User = require('../src/User');
+const userData = require('./test-data/user-test-data');
+//an array of various users
+const hydrationData = require('./test-data/hydration-test-data');
+//this is an array of objects with ids to mark who owns what.
+
+describe('Hydration', () => {
+  let hydration, user;
+  beforeEach(() => {
+    hydration = new Hydration();
+    user = new User(userData[2]);
+  });
+
+  it('', () => {
+
+  })
+
+  it('', () => {
+
+  })
+
+  it('', () => {
+
+  })
+
+  it('', () => {
+
+  })
+
+  it('', () => {
+
+  })
+
+  it('', () => {
+
+  })
+
+  it('', () => {
+
+  })
+
+  it('', () => {
+
+  })
+
+  it('', () => {
+
+  })
+
+  it('', () => {
+
+  })
+
+  it('', () => {
+
+  })
+
+  it('', () => {
+
+  })
+
+})
