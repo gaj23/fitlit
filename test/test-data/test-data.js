352fcd99e79da6970039125e9e3baa3b62c9dbba
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2020-12-09 20:39:01 -0500

    Reorganize file structure so all test-data files are inside of the test file

---
+++ test/test-data/test-data.js
@@ -0,0 +1,73 @@
+const userData = [{
+    "id": 1,
+    "name": "Harry Potter",
+    "address": "4 Privet Drive, Little Whinging, Surrey, England, Great Britain",
+    "email": "harrypotter@hogwarts.com",
+    "strideLength": 5.5,
+    "dailyStepGoal": 20000,
+    "friends": [
+      16,
+      4,
+      8
+    ]
+  },
+  {
+    "id": 2,
+    "name": "Ron Weasly",
+    "address": "The Burrows",
+    "email": "ronweasly@hogwarts.com",
+    "strideLength": 5.2,
+    "dailyStepGoal": 10000,
+    "friends": [
+      9,
+      18,
+      24,
+      19
+    ]
+  },
+  {
+    "id": 3,
+    "name": "Hermonie Granger",
+    "address": "Hampstead, England, Great Britain",
+    "email": "hermoniegranger@hogwarts.com",
+    "strideLength": 4.4,
+    "dailyStepGoal": 15000,
+    "friends": [
+      19,
+      11,
+      42,
+      33
+    ]
+  },
+  {
+    "id": 4,
+    "name": "Sirius Black",
+    "address": "Number 12, Grimmauld Place",
+    "email": "siriusblack@hogwarts.com",
+    "strideLength": 3.5,
+    "dailyStepGoal": 5000,
+    "friends": [
+      48,
+      7,
+      44,
+      8
+    ]
+  },
+  {
+    "id": 5,
+    "name": "Draco Malfoy",
+    "address": " Wiltshire, England",
+    "email": "dracomalfoy@hogwarts.com",
+    "strideLength": 4.6,
+    "dailyStepGoal": 8000,
+    "friends": [
+      13,
+      44,
+      49,
+      33,
+      10
+    ]
+  }
+];
+
+module.exports = userData;
