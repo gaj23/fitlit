8f78ea8f584171b71cb8628f87a4743ea35ae1c9
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2020-12-10 21:01:11 -0500

    Complete class to pass tests

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,4 +1,30 @@
-class Hydration() {
+class Hydration {
+  constructor(data) {
+    this.data = data;
+    this.userHydrationData = [];
+  }
+
+  getUserHydrationData(id) {
+    return this.data.filter(data =>
+      data.userID === id)
+  }
+
+  calculateDailyAvgIntake(id) {
+    return this.getUserHydrationData(id).reduce((acc, user) => {
+      acc += user.numOunces;
+      return acc
+    }, 0) / this.getUserHydrationData(id).length
+  }
+
+  findDailyIntake(date, id) {
+    return this.getUserHydrationData(id).find(day => day.date === date).numOunces;
+  }
+
+  getWeeklyIntake(date, id) {
+    return this.getUserHydrationData(id).slice(date, 7).map(user => {
+      return user.numOunces;
+    })
+  }
 
 }
 
