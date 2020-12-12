d71a9d88b1737e8216adf4d59d8d859b80126233
Thao Ma <MaThao@gmail.com>
2020-12-11 20:53:10 -0700

    Update getWeekelyIntake() in Hydration.js

--- src/Hydration.js
+++ src/Hydration.js
@@ -7,22 +7,22 @@ class Hydration {
     return helper.getUserData(this.hydrationData, id);
   }
 
-  // calculateDailyAvgIntake() {
-  //   return this.userHydrationData.reduce((acc, user) => {
-  //     acc += user.numOunces;
-  //     return acc
-  //   }, 0) / this.userHydrationData.length
-  // }
+  calculateDailyAvgIntake() {
+    return this.getUserHydrationData(this.hydrationData).reduce((acc, user) => {
+      acc += user.numOunces;
+      return acc
+    }, 0) / this.getUserHydrationData(this.hydrationData).length
+  }
 
   findDailyIntake(date, id) {
     return this.getUserHydrationData(id).find(day => day.date === date).numOunces;
   }
 
-  // getWeeklyIntake(date) {
-  //   return this.userHydrationData.slice(date, 7).map(user => {
-  //     return user.numOunces;
-  //   })
-  // }
+  getWeeklyIntake(date, id) {
+    return this.getUserHydrationData(id).slice(date, 7).map(user => {
+      return user.numOunces;
+    })
+  }
 
 }
 
--- src/helper.js
+++ src/helper.js
@@ -1,9 +1,15 @@
 const helper = {
   getUserData: (userData, id) => {
     return userData.filter(data => data.userID === id);
-  }
-}
+  },
 
+//   calculateDailyAvg: (userData) => {
+//     return userData.reduce((acc, user) => {
+//       acc += user.
+//     })
+//   }
+// }
+}
 
 if (typeof module !== "undefined") {
   module.exports = helper;
