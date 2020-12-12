b0e2dfa531ac97b9e712cc269672dab55e9cbb11
Thao Ma <MaThao@gmail.com>
2020-12-12 12:51:27 -0700

    Add all necessary requires

---
+++ test/sleep-test.js
@@ -0,0 +1,7 @@
+const chai = require("chai");
+const expect = chai.expect;
+
+const Sleep = require('../src/sleep');
+const sleepData = require('./test-data/sleep-test-data');
+const helper = require('../src/helper');
+
