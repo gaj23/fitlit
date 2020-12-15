5069e8326c0c9d3b8c10f39627ce041fc4f0d6a9
Thao Ma <MaThao@gmail.com>
2020-12-15 16:38:27 -0700

    add requires to test file

---
+++ test/ActivityRepository-test.js
@@ -0,0 +1,6 @@
+const chai = require("chai");
+const expect = chai.expect;
+
+const activityRepo = require('../src/ActivityRespository');
+const activityData = require('./test-data/activity-test-data');
+const helper = require('../src/helper');
