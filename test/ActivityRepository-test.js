95a71a2ed15643a29cd93f7baa2e09d88a14bb7b
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2020-12-16 08:31:39 -0500

    Remove only to prevent false positive when testing

--- test/ActivityRepository-test.js
+++ test/ActivityRepository-test.js
@@ -11,15 +11,15 @@ describe('ActivityRepo', () => {
     activityRepo = new ActivityRepo(activityData);
   });
 
-  it.only('should calculate average stairs climbed for all users for a specific date', () => {
+  it('should calculate average stairs climbed for all users for a specific date', () => {
     expect(activityRepo.getAvgStairsClimbedForAllUsers('2019/06/15')).to.equal('19.7');
   });
 
-  it.only('should calculate average steps taken for all user for a specific date', () => {
+  it('should calculate average steps taken for all user for a specific date', () => {
     expect(activityRepo.getAvgStepsForAllUsers('2019/06/15')).to.equal(5091);
   });
 
-  it.only('should calculate average minutes active for all user on a specific date', () => {
+  it('should calculate average minutes active for all user on a specific date', () => {
     expect(activityRepo.getAvgMinActiveForAllUsers('2019/06/15')).to.equal('131.33');
   });
 });
