dd40580b87c877d26fe46d5d2c3278cb92562aeb
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2020-12-16 13:32:09 -0500

    Create test for user day mileage

--- test/Activity-test.js
+++ test/Activity-test.js
@@ -1,29 +1,34 @@
 const chai = require("chai");
 const expect = chai.expect;
 
+
+const UserRepo = require('../src/UserRepository');
+const userData = require('./test-data/user-test-data')
 const Activity = require('../src/Activity');
 const activityData = require('./test-data/activity-test-data');
 const helper = require('../src/helper');
 
 describe('Activity', () => {
-
-  let activity;
+  let activity, user;
 
   beforeEach(() => {
-    activity = new Activity(activityData);
-    helper.getUserData(sleepData, 2);
-  })
-
-  it.skip('should calculate miles a user walked on a chosen day', () => {
+    helper.getUserData(userData, 2);
+    activity = new Activity(activityData, userData);
+    helper.getUserData(activityData, 2);
 
   })
 
-  it.skip('should return how many minutes a user was active on a chosen day', () => {
-
+  it('should calculate miles a user walked on a chosen day', () => {
+    expect(activity.calculateMiles('2019/06/15', 3)).to.equal(6.2);
   })
 
-  it.skip('should return the average minutes of activity during a week', () => {
+  it('should return how many minutes a user was active on a chosen day', () => {
+    expect(activity.getActiveMins('2019/06/15', 3)).to.equal(116);
+  })
 
+  it('should return the average minutes of activity during a week', () => {
+    expect(activity.calculateWeeklyActiveMins('2019/06/15', 3)).to.equal(165);
+    expect(activity.calculateWeeklyActiveMins('2019/06/15', 1)).to.equal(171);
   })
 
   it.skip('should notify a user if they reached their step goal', () => {
