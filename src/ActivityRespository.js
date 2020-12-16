4fabe102b2646448cd14909dac5cd2dcc5c7a621
Thao Ma <MaThao@gmail.com>
2020-12-15 18:33:27 -0700

    Create & pass test to calculate getAvgStepsForAllUsers()

--- src/ActivityRespository.js
+++ src/ActivityRespository.js
@@ -10,7 +10,16 @@ class ActivityRepo {
       return acc += data.flightsOfStairs;
      } , 0) / helper.getSpecificDay(this.activityData, date).length).toFixed(1);
   }
+
+  getAvgStepsForAllUsers(date) {
+    return Math.round(helper.getSpecificDay(this.activityData, date).reduce((acc, data) => {
+      return acc += data.numSteps;
+     } , 0) / helper.getSpecificDay(this.activityData, date).length);
+  }
+
 }
+
+
 
 if (typeof module !== "undefined") {
   module.exports = ActivityRepo;
--- test/ActivityRepository-test.js
+++ test/ActivityRepository-test.js
@@ -11,7 +11,12 @@ describe('ActivityRepo', () => {
     activityRepo = new ActivityRepo(activityData);
   });
 
-  it.only('should calculate avg stairs climbed for all users for a specific date', () => {
+  it.only('should calculate average stairs climbed for all users for a specific date', () => {
     expect(activityRepo.getAvgStairsClimbedForAllUsers('2019/06/15')).to.equal('19.7');
-  })
+  });
+
+  it.only('should calculate average steps taken for all user for a specific date', () => {
+    expect(activityRepo.getAvgStepsForAllUsers('2019/06/15')).to.equal(5091);
+  });
+
 })
