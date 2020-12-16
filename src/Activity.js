7de7f670510a60ac0baba1faa8b8758c674c0589
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2020-12-16 13:32:48 -0500

    Create calculateMiles to pass test file assertions

--- src/Activity.js
+++ src/Activity.js
@@ -1,9 +1,33 @@
 const helper = require('../src/helper');
-
+// const UserRepository = require('../src/UserRepository');
+// const User = require('../src/User');
+//need user data, too because then have user stride length and step goal
 class Activity {
-  constructor(activityData) {
+  constructor(activityData, userData) {
     this.activityData = activityData;
+    this.userData = userData;
   }
+
+  getUserActivityData(id) {
+    return helper.getUserData(this.activityData, id);
+  }
+
+  calculateMiles(date, id) {
+    const user = this.userData.find(user => user.id === id);
+    const specificDateActivity = helper.getSpecificDay(this.getUserActivityData(id), date);
+    return parseFloat((user.strideLength * specificDateActivity[0].numSteps / 5280).toFixed(1));
+  }
+
+  getActiveMins(date, id) {
+    return helper.getSpecificDay(this.getUserActivityData(id), date)[0].minutesActive;
+  }
+
+  calculateWeeklyActiveMins(date, id) {
+    return Math.round(helper.getSpecificWeek(this.getUserActivityData(id), date).reduce((acc, day) => {
+      return acc += day.minutesActive;
+    }, 0) / (helper.getSpecificWeek(this.getUserActivityData(id), date)).length)
+  }
+
 }
 
 if (typeof module !== "undefined") {
