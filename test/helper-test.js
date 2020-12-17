4219242c723213082d831ff682feb182ba01a966
Thao Ma <MaThao@gmail.com>
2020-12-17 10:27:54 -0700

    Add test for helper getSpecificWeek method

--- test/helper-test.js
+++ test/helper-test.js
@@ -51,4 +51,50 @@ describe('helper', () => {
       },
     ]);
   });
+
+  it('should be able to get specific user weekly data', () => {
+    expect(helper.getSpecificWeek(sleepData, '2019/06/15')).to.deep.equal([{
+        "date": "2019/06/15",
+        "hoursSlept": 6.1,
+        "sleepQuality": 2.2,
+        "userID": 1
+      },
+      {
+        "date": "2019/06/15",
+        "hoursSlept": 7,
+        "sleepQuality": 4.7,
+        "userID": 2
+      },
+      {
+        "date": "2019/06/15",
+        "hoursSlept": 10.8,
+        "sleepQuality": 4.7,
+        "userID": 3
+      },
+      {
+        "date": "2019/06/15",
+        "hoursSlept": 5.4,
+        "sleepQuality": 3,
+        "userID": 4
+      },
+      {
+        "date": "2019/06/15",
+        "hoursSlept": 4.1,
+        "sleepQuality": 3.6,
+        "userID": 5
+      },
+      {
+        "date": "2019/06/16",
+        "hoursSlept": 4.1,
+        "sleepQuality": 3.8,
+        "userID": 1
+      },
+      {
+        "date": "2019/06/16",
+        "hoursSlept": 7.5,
+        "sleepQuality": 3.8,
+        "userID": 2
+     },
+    ]);
+  });
 });
