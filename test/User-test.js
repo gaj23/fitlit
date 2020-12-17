1f1ef3e0883b414ab0e7cb63a0cdade701f26e4e
Thao Ma <MaThao@gmail.com>
2020-12-17 13:50:34 -0700

    Edit User-test and User-repository-test class syntax

--- test/User-test.js
+++ test/User-test.js
@@ -9,39 +9,38 @@ describe('User', () => {
 
   beforeEach(() => {
     user = new User(userData[2]);
-  })
+  });
 
   it('should have an user id', () => {
-    expect(user.id).to.equal(userData[2].id)
-  })
+    expect(user.id).to.equal(userData[2].id);
+  });
 
   it('should have an user name', () => {
     expect(user.name).to.equal(userData[2].name);
-  })
+  });
 
   it('should have an user address', () => {
     expect(user.address).to.equal(userData[2].address);
-  })
+  });
 
   it('should have an user email', () => {
     expect(user.email).to.equal(userData[2].email);
-  })
+  });
 
   it('should have an user\'s stride length', () => {
     expect(user.strideLength).to.equal(userData[2].strideLength);
-  })
+  });
 
   it('should have an user\'s daily step goal', () => {
     expect(user.dailyStepGoal).to.equal(userData[2].dailyStepGoal);
-  })
+  });
 
   it('should have an user\'s friends', () => {
     expect(user.friends).to.equal(userData[2].friends);
-  })
+  });
 
   it('should return user\'s first name', () => {
     expect(user.returnFirstName).to.be.a('function');
     expect(user.returnFirstName()).to.deep.equal('Hermonie');
-  })
-
-})
+  });
+});
