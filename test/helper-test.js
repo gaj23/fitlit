77cb7f07d674078c292aefa1141de978910bd8a7
Thao Ma <MaThao@gmail.com>
2020-12-17 10:28:26 -0700

    Add test for helper getSpecificDay method

--- test/helper-test.js
+++ test/helper-test.js
@@ -97,4 +97,38 @@ describe('helper', () => {
      },
     ]);
   });
+  
+  it('should be able to get specific day data', () => {
+    expect(helper.getSpecificDay(sleepData, '2019/06/15')).to.deep.equal([{
+        "date": "2019/06/15",
+        "hoursSlept": 6.1,
+        "sleepQuality": 2.2,
+        "userID": 1
+      },
+      {
+        "date": "2019/06/15",
+        "hoursSlept": 7,
+        "sleepQuality": 4.7,
+        "userID": 2
+      },
+      {
+        "date": "2019/06/15",
+        "hoursSlept": 10.8,
+        "sleepQuality": 4.7,
+        "userID": 3
+      },
+      {
+        "date": "2019/06/15",
+        "hoursSlept": 5.4,
+        "sleepQuality": 3,
+        "userID": 4
+      },
+      {
+        "date": "2019/06/15",
+        "hoursSlept": 4.1,
+        "sleepQuality": 3.6,
+        "userID": 5
+      },
+    ]);
+  });
 });
